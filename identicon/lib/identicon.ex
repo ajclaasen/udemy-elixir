b16e4a925e641fd7569d89d9904e62895f771b95
rjclaasen <raphael.claasen@gmail.com>
2019-04-30 19:59:29 +0200

    Build mirrored grid from hashed seed

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -6,12 +6,30 @@ defmodule Identicon do
   @doc """
     Generates an identicon from a given seed
   """
-  def generate_identicon(seed) do
+  def generate(seed) do
     seed
     |> hash_string
     |> pick_color
+    |> build_grid
   end
 
+  def build_grid(%Identicon.Image{hex: hex} = image) do
+    hex
+    |> Enum.chunk_every(3, 3, :discard)
+    |> Enum.map(&mirror_row/1)
+  end
+
+  @doc """
+
+  """
+  def mirror_row(row) do
+    [first, second | _tail] = row
+    row ++ [second, first]
+  end
+
+  @doc """
+    Adds the first three bytes as the RGB values to the Identicon.Image
+  """
   def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
     %Identicon.Image{image | color: {r, g, b}}
   end
