1c0625e320aa79447f41fd8ae2a92a04edb34f9d
rjclaasen <raphael.claasen@gmail.com>
2019-04-30 15:04:20 +0200

    Add barebones Image struct and extract color from hash

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -1,21 +1,31 @@
 defmodule Identicon do
   @moduledoc """
-    Contains methods for generating an identicon and saving it.
+    Contains methods for generating an identicon and saving it
   """
 
   @doc """
-    Generates an identicon from a given seed.
+    Generates an identicon from a given seed
   """
   def generate_identicon(seed) do
     seed
     |> hash_string
+    |> pick_color
+  end
+
+  def pick_color(image) do
+    %Identicon.Image{hex: hex_list} = image
+    [r, g, b | _tail] = hex_list
+
+    [r, g, b]
   end
 
   @doc """
-    Generates a list of 16 bytes by hashing a string using MD5.
+    Hashes a string using MD5 and returns an Identicon.Image
   """
   def hash_string(string) do
-    :crypto.hash(:md5, string)
+    hex = :crypto.hash(:md5, string)
     |> :binary.bin_to_list
+
+    %Identicon.Image{hex: hex}
   end
 end
---
+++ identicon/lib/image.ex
@@ -0,0 +1,7 @@
+defmodule Identicon.Image do
+  @moduledoc """
+    Contains an Image struct representing an Identicon.
+  """
+
+  defstruct hex: nil
+end
