62e6f64e4e114f736166094eb93c7cb67aae8712
rjclaasen <raphael.claasen@gmail.com>
2019-05-22 01:05:06 +0200

    Create new topic form

--- discuss/web/controllers/topic_controller.ex
+++ discuss/web/controllers/topic_controller.ex
@@ -6,6 +6,10 @@ defmodule Discuss.TopicController do
   def new(conn, params) do
     changeset = Topic.changeset(%Topic{})
 
-    render conn, "new.html"
+    render conn, "new.html", changeset: changeset
+  end
+
+  def create(conn, params) do
+
   end
 end
