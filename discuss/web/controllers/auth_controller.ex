4f2280ffcf8e01e6dcca8ccd7498931ea2963bd9
rjclaasen <raphael.claasen@gmail.com>
2019-05-27 00:52:25 +0200

    Create sign out functionality

--- discuss/web/controllers/auth_controller.ex
+++ discuss/web/controllers/auth_controller.ex
@@ -11,6 +11,12 @@ defmodule Discuss.AuthController do
     sign_in(conn, changeset)
   end
 
+  def signout(conn, _params) do
+    conn
+    |> configure_session(drop: true)
+    |> redirect(to: topic_path(conn, :index))
+  end
+
   defp sign_in(conn, changeset) do
     case insert_or_read_user(changeset) do
       {:ok, user} ->
